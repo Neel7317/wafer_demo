6e678aadc442e05d436688da00d2e0c57df8640b
Neel7317 <65647192+Neel7317@users.noreply.github.com>
2022-08-09 16:29:44 +0530

    config added

---
+++ src/pipeline_01_data_preparation.py
@@ -0,0 +1,17 @@
+import argparse
+from ast import arg
+from email.policy import default
+import os
+import argparse
+import yaml
+import logging
+
+
+
+if __name__=="__main__":
+    args=argparse.ArgumentParser()
+    args.add_argument("--config",default=default)
+    args.add_argument("--datasource",default=None)
+
+    parse_args=args.parse_args()
+    print(parse_args.config,parse_args.datasource)
